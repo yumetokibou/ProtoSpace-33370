c76856688edef7599755561f9004a3b0a5761124
yumetokibou <77270061+yumetokibou@users.noreply.github.com>
2021-03-27 20:07:02 +0900

    protoype_controller.rbの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user! ,only: [:new, :edit, :destroy]
   before_action :set_prototype, only: [:edit]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:new,:index, :show]
 
   def index
     @prototype = Prototype.all
@@ -58,6 +58,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
+      @prototype = Prototype.new
     unless current_user == @prototype.user
       redirect_to action: :index
     
